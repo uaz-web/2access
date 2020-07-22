dc0f1610016b386e7a1b4338739f0650af85d1bd
Kevin Cooper <kevincooper@arizona.edu>
2020-07-22 14:10:28 -0700

    updates

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `rebeccapurple`,
+      background: `#ab0520`,
       marginBottom: `1.45rem`,
     }}
   >
