4db2f0e9f2b9453627a76b9b8e41301747f97e8e
Kevin Cooper <kevincooper@arizona.edu>
2020-07-30 14:56:15 -0700

    update homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,28 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import AlertBox from "../components/alertbox"
+import ServiceBox from "../components/servicebox"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="UAccess" />
     <h1>UAccess</h1>
+
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2>Alerts</h2>
+          <AlertBox />
+        </div>
+
+        <div class="col">
+          <h2>Services</h2>
+          <ServiceBox />
+        </div>
+      </div>
+    </div>
+
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
