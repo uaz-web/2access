0ccc13b843aebfec11af5725d5efd9221704aad6
Kevin Cooper <kevincooper@arizona.edu>
2020-07-30 15:34:32 -0700

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -2,11 +2,27 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
-const Footer = ({ siteTitle }) => (
-  <footer>
-    © {new Date().getFullYear()}, Built with
-    {` `}
-    <a href="https://www.gatsbyjs.org">Gatsby</a>
+const Footer = ({ children }) => (
+  <footer className="fixed-bottom" style={{background: '#f4ede5'}}>
+    <div class="container">
+      {/* <hr /> */}
+      <br />
+      <div class="d-flex flex-row justify-content-center">
+        { children }
+      </div>
+      <div class="d-flex flex-row justify-content-center">
+        <div class="p-2">
+          <a href="https://privacy.arizona.edu/privacy-statement">
+            University Privacy Statement
+          </a>
+        </div>
+      </div>
+      <div class="d-flex flex-row justify-content-center">
+        <div class="p-2">
+          © {new Date().getFullYear()} The Arizona Board of Regents | The University of Arizona | Tucson, Arizona 85721 | 520-621-2211
+        </div>
+      </div>
+    </div>
   </footer>
 )
 
