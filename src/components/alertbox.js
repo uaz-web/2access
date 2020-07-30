c7376c6b8507e1b233ec9bfa88f130faa4c20637
Kevin Cooper <kevincooper@arizona.edu>
2020-07-30 14:55:44 -0700

    more updates

--- src/components/alertbox.js
+++ src/components/alertbox.js
@@ -37,7 +37,7 @@ class AlertBox extends React.Component {
     if (error) {
       return <div>Error: {error.message}</div>;
     } else if (!isLoaded) {
-      return <div>Loading...</div>;
+      return <div>Loading Alerts...</div>;
     } else {
       return (
         <div>
